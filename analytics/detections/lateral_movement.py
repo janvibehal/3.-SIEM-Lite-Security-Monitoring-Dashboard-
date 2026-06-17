2598524307b940a7d9ac85092e76997b03c857d4
Jiadholpuria <145265372+Jiadholpuria@users.noreply.github.com>
2026-06-17 21:42:13 +0530

    Implemented SIEM analytics detections and ML prototype

--- analytics/detections/lateral_movement.py
+++ analytics/detections/lateral_movement.py
@@ -1,2 +1,21 @@
+import json
+from collections import defaultdict
+
 def detect_lateral_movement():
-    print("Checking lateral movement attacks...")
+    with open("logs.json", "r") as file:
+        logs = json.load(file)
+
+    user_ips = defaultdict(set)
+
+    for log in logs:
+        if log.get("event_type") == "network_access":
+            user = log.get("user")
+            ip = log.get("ip")
+
+            user_ips[user].add(ip)
+
+    for user, ips in user_ips.items():
+        if len(ips) > 2:
+            print("\n🚨 ALERT: Possible Lateral Movement")
+            print(f"User: {user}")
+            print(f"Systems Accessed: {len(ips)}")
--- analytics/detections/privilege_escalation.py
+++ analytics/detections/privilege_escalation.py
@@ -1,2 +1,10 @@
+import json
+
 def detect_privilege_escalation():
-    print("Checking privilege escalation attacks...")
+    with open("logs.json", "r") as file:
+        logs = json.load(file)
+
+    for log in logs:
+        if log.get("event_type") == "privilege_escalation":
+            print("\n🚨 ALERT: Privilege Escalation Detected")
+            print(f"User: {log.get('user')}")
